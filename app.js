aee8f2abeab0db5590384bfac83684626b8391c0
Himansu01-sharma <himansusharma0928@gmail.com>
2025-10-01 02:13:46 +0530

    the

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 let boxes=document.querySelectorAll(".box");
 let restbtn= document.querySelector(".reser-btn");
 let turn0 = true; // player x and player 0 is there 
+
+const winPattern =[
+[0,1,2],
+[0,3,6],
+[0,4,8],
+[1,4,7],
+[2,5,8],
+[2,4,6],
+[3,4,5],
+[6,7,8],
+];
+
+boxes.forEach(  (box)=>{
+    box.addEventListener("click",()=>{
+        console.log(" button was clicked");
+        box.innerText="x";
+        if(turn0){
+            // player 0 turn
+            box.innerText="0";
+            turn0=false;
+        }
+        else{
+            //player x turn 
+            box.innerText="x";
+            turn0=true;
+        }
+        box.disabled=true;
+        checkWinner();
+    });
+});
+
+const checkWiner=()=>{
+    for(pattern of winPattern){
+        console.log(pattern);
+    }
+}
+
